5821c48c735f5fa5fa62b303547f51832b7cd231
wildenali <wildeeeeen@gmail.com>
2019-03-15 21:45:28 +0700

    cara membuat publisher dan subscriber dengan cpp

---
+++ 01_Beginner/ae_publisher_and_subscriber_cpp/src/penerima_subscriber.cpp
@@ -0,0 +1,24 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+// ini program untuk menerima pesan dari pengirim (publisher)
+
+// fungsi callback yang akan di panggil ketika si publisher ngirim message
+void obrolanCallback(const std_msgs::String::ConstPtr& msg) {
+  ROS_INFO("I heard:[%s]", msg->data.c_str());
+}
+
+int main(int argc, char **argv) {
+
+  ros::init(argc, argv, "sipenerima");
+
+  ros::NodeHandle n;
+
+  // 1000 adalah queue size, jika queue mencapai 1000 pesan, we will start throwing away old messages as new ones arrive
+  ros::Subscriber sub = n.subscribe("obrolan", 1000, obrolanCallback);
+
+  ros::spin();
+
+
+  return 0;
+}
