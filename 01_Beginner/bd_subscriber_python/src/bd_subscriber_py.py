4fc531228c1abca8b338a6746b53c3f5ce43f5db
wildenali <wildeeeeen@gmail.com>
2019-04-12 23:21:49 +0700

    nge subscribe si bc_publisher_python pakage dengan python language

---
+++ 01_Beginner/bd_subscriber_python/src/bd_subscriber_py.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import rospy
+from std_msgs.msg import String
+
+def callback_terima_dari_bb_pub_py(msg):
+    rospy.loginfo("Message received: ")
+    rospy.loginfo(msg)
+
+if __name__ == '__main__':
+    rospy.init_node('bb_subscriber_py_node')
+
+    # nge subscriber si /bb_pub_py
+    pub = rospy.Subscriber("/bb_pub_py", String, callback_terima_dari_bb_pub_py)   # queue_size untuk buffer, kalau misalnya data yg dikirim banyaaaaaak banget, dan si subscriber tidak bisa membaca semua, maka perlu queue_size
+
+    rospy.spin()
