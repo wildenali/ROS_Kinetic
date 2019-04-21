1ca43892c175ca5f33145a6abe86c6f7500cb6cf
wildenali <wildeeeeen@gmail.com>
2019-04-21 11:46:46 +0700

    membuat activity action client dengan python, nge handle si goal

---
+++ 02_Intermediate/bb_my_robot_tutorials/src_section5/activity_action_client.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+
+import rospy
+import actionlib
+
+from ba_my_robot_msgs.msg import CountUntilAction
+from ba_my_robot_msgs.msg import CountUntilGoal
+
+class CountUntilClient:
+    def __init__(self):
+        self._ac = actionlib.ActionClient('/count_until', CountUntilAction)
+        self._ac.wait_for_server()
+        rospy.loginfo("Action server up, we can send goals")
+        self._goal_handles = {}
+
+    def on_transition(self, goal_handle):
+        index = 0
+        for i in self._goal_handles:
+            if self._goal_handles[i] == goal_handle:
+                index = i
+                break
+        rospy.loginfo("--- INI Transition callback")
+
+        # rospy.loginfo(goal_handle.get_comm_state())
+        # rospy.loginfo(goal_handle.get_goal_status())
+        if goal_handle.get_comm_state() == 2:
+            rospy.loginfo(str(index) + ": Goal just went active")
+        if goal_handle.get_comm_state() == 7:
+            rospy.loginfo(str(index) + ": Goal is DONE")
+            rospy.loginfo(goal_handle.get_terminal_state())
+            rospy.loginfo(goal_handle.get_result())
+
+    def on_feedback(self, goal_handle, feedback):
+        # rospy.loginfo("--- INI Feedback callback")
+        # rospy.loginfo(feedback)
+        pass
+
+    def send_goal(self, max_number, wait_duration):
+        goal = CountUntilGoal()
+        goal.max_number = max_number
+        goal.wait_duration = wait_duration
+        rospy.loginfo("Send goal")
+        # thing = self._ac.send_goal(goal, self.on_transition, self.on_feedback)
+        # rospy.loginfo(thing)
+        goal_handle = self._ac.send_goal(goal, self.on_transition, self.on_feedback)
+        # rospy.loginfo(goal_handle)
+
+        return goal_handle
+
+if __name__ == '__main__':
+    rospy.init_node('count_until_client')
+    client = CountUntilClient()
+
+    # send goal
+    # client.send_goal(6 ,0.5)        # kenapa 6, karena itu dari nilai maksimal dari file2 yg lain coba cekcek ricek
+    # goal_handle = client.send_goal(6 ,0.5)        # kenapa 6, karena itu dari nilai maksimal dari file2 yg lain coba cekcek ricek
+
+    goal_handle1 = client.send_goal(6 ,0.5)        # kenapa 6, karena itu dari nilai maksimal dari file2 yg lain coba cekcek ricek
+    client._goal_handles['1'] = goal_handle1
+
+    goal_handle2 = client.send_goal(5 ,0.5)
+    client._goal_handles['1'] = goal_handle3
+
+    rospy.sleep(1.0)
+    goal_handle1.cancel()
+
+    rospy.spin()
